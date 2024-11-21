eca24a22f49cb7482b8502a99f26f22505e3ee24
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-11-21 11:26:43 -0800

    added mystery function, added 3 additional tests

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,22 @@
-export const mystery3 = (string1, string2) => {};
+export const mystery3 = (string1, string2) => {
+  let difCount = 0;
+  if (string1 === string2) {
+    return 0;
+  }
+  let i = 0
+  do {
+    if (string1[i] !== string2[i]) {
+      difCount += 1;
+    }
+    i++;
+  }
+  while (i < string1.length & string2.length)
+
+  if (string1.length > i) {
+    difCount += (string1.length - i);
+  } else if (string2.length > i) {
+    difCount += (string2.length -i);
+  }
+
+  return difCount;
+};
--- test/mystery.test.js
+++ test/mystery.test.js
@@ -47,3 +47,30 @@ test('Comparing "GAGCCTACTAACGGGAT" and "CATCGTAATGACGGCCT" returns 7', () => {
   // Assert
   expect(result).toEqual(7);
 });
+
+test('comparing "GAGCC" and "GAGCCMMNL" returns 4', () => {
+  const string1 = 'GAGCC';
+  const string2 = 'GAGCCMMNL';
+
+  const result = mystery3(string1, string2);
+  
+  expect(result).toEqual(4)
+});
+
+test('one empty string: "" for string one and "GAGCC" for string two returns 5', () => {
+  const string1 = '';
+  const string2 = 'GAGCC';
+
+  const result = mystery3(string1, string2);
+  
+  expect(result).toEqual(5)
+});
+
+test('one empty string: "GAGCC" for string one and "" for string two returns 5', () => {
+  const string1 = 'GAGCC';
+  const string2 = '';
+
+  const result = mystery3(string1, string2);
+  
+  expect(result).toEqual(5)
+});
